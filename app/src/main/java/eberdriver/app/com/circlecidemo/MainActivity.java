3506dc22fd9dc5b1f127dbb2ccf08dee3065658c
unknown <avaiyakirtib@gmail.com>
2017-11-13 14:14:24 +0530

    This is a new commit for what I originally planned to be amended

---
+++ app/src/main/java/eberdriver/app/com/circlecidemo/MainActivity.java
@@ -0,0 +1,13 @@
+package eberdriver.app.com.circlecidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
