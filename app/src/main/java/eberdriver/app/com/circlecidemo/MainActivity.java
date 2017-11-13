c22e38b48ac3a7daa49a01354e0a3d5b703b5c6f
psk56 <teampsk1@gmail.com>
2017-11-13 14:45:09 +0530

    Test

--- app/src/main/java/eberdriver/app/com/circlecidemo/MainActivity.java
+++ app/src/main/java/eberdriver/app/com/circlecidemo/MainActivity.java
@@ -2,6 +2,7 @@ package eberdriver.app.com.circlecidemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView=new TextView(this);
     }
 }
